78afb99cde52cb11b946e1f5c290df3991256369
Samuel Tran <samuel.tran@liferay.com>
2017-11-09 16:33:55 -0800

    Set jenkins url

---
+++ jenkins/jenkins_home/init.groovy
@@ -0,0 +1,5 @@
+def env = System.getenv()
+
+def ant = new AntBuilder()
+
+ant.replace(file: "/var/jenkins_home/config.xml", token: "JENKINS_PORT_8080_TCP_ADDR", value: env["JENKINS_PORT_8080_TCP_ADDR"])
