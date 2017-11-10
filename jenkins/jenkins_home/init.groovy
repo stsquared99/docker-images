a9aa76f97b1cf97e36597be9ef5f5aba41b32c85
Samuel Tran <samuel.tran@liferay.com>
2017-11-09 16:43:01 -0800

    Reload config

--- jenkins/jenkins_home/init.groovy
+++ jenkins/jenkins_home/init.groovy
@@ -3,3 +3,5 @@ def env = System.getenv()
 def ant = new AntBuilder()
 
 ant.replace(file: "/var/jenkins_home/config.xml", token: "JENKINS_PORT_8080_TCP_ADDR", value: env["JENKINS_PORT_8080_TCP_ADDR"])
+
+jenkins.model.Jenkins.instance.reload()
